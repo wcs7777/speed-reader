5e32ad0295a28e4f7a91ee0ffcba484102e67b6b
Willian Carlos <wcs777git@outlook.com>
2023-02-11 18:17:04 -0300

    add findInRanges()

--- js/utils/alphanumeric.js
+++ js/utils/alphanumeric.js
@@ -65,3 +65,24 @@ export function splitWords(paragraph) {
 export function chunkMilliseconds(chunkLength, charactersPerSecond) {
 	return chunkLength / charactersPerSecond * 1000;
 }
+
+/**
+ * @param {{ begin: number, end: number }[]} ranges
+ * @param {number} value
+ * @returns {number}
+ */
+export function findInRanges(ranges, value) {
+	let begin = 0;
+	let end = ranges.length - 1;
+	while (begin <= end) {
+		const middle = parseInt((begin + end) / 2);
+		if (value > ranges[middle].end) {
+			begin = middle + 1;
+		} else if (value < ranges[middle].begin) {
+			end = middle - 1;
+		} else {
+			return middle;
+		}
+	}
+	return -1;
+}
