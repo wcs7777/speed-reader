734f50ce2dfd7dcce0603b49efe1858e995302c0
Willian Carlos <wcs777git@outlook.com>
2023-02-11 22:58:53 -0300

    begin refactor of SpeedReader component

--- js/components/SpeedReader.js
+++ js/components/SpeedReader.js
@@ -1,161 +1,70 @@
-import ChunkText from "./ChunkText.js";
-import ParagraphSpeedReader from "./ParagraphSpeedReader.js";
-import {
-	chunkMilliseconds,
-	splitParagraphChunks,
-	splitParagraphs
-} from "../utils/alphanumeric.js";
-import BoundedList from "../utils/BoundedList.js";
-import { sleep } from "../utils/mixed.js";
+import { createTemplate, tag } from "../utils/dom.js";
+import defaultSettings from "../utils/defaultSettings.js";
+import { ChunkText } from "./ChunkText.js";
 
-export default class SpeedReader extends HTMLDivElement {
-	static get customTagName() {
-		return "speed-reader";
-	}
+const attrs = {
+	isOpened: "data-is-opened",
+	isPaused: "data-is-paused",
+};
+const cssVariables = {
+	highlightColor: ChunkText.cssVariables.highlightColor,
+	backgroundColor: "--speed-reader-background-color",
+	textColor: "--speed-reader-text-color",
+	fontSize: "--speed-reader--font-size",
+	lineHeight: "--speed-reader--line-height",
+	fontFamily: "--speed-reader--font-family",
+	textAlign: "--speed-reader--text-align",
+};
+const template = createTemplate(`
+<style>
+	:host {
+		${cssVariables.highlightColor} = ${defaultSettings.highlightColor};
+		${cssVariables.backgroundColor} = ${defaultSettings.backgroundColor};
+		${cssVariables.textColor} = ${defaultSettings.textColor};
+		${cssVariables.fontSize} = ${defaultSettings.fontSize};
+		${cssVariables.lineHeight} = ${defaultSettings.lineHeight};
+		${cssVariables.fontFamily} = ${defaultSettings.fontFamily};
+		${cssVariables.textAlign} = ${defaultSettings.textAlign};
 
-	static get extendingTagName() {
-		return "div";
+		background-color: var(${cssVariables.backgroundColor});
+		text-color: var(${cssVariables.textColor});
+		font-size: var(${cssVariables.fontSize});
+		line-height: var(${cssVariables.lineHeight});
+		font-family: var(${cssVariables.fontFamily});
+		text-align: var(${cssVariables.textAlign});
 	}
+</style>
+<slot></slot>
+`);
 
-	static get averageWordSize() {
-		return 5.7;
-	}
-
-	static build(text, wordsPerMinute, wordsPerChunk) {
-		return document.createElement(
-			SpeedReader.extendingTagName,
-			{ is: SpeedReader.customTagName },
-		)
-			.newText(text, wordsPerMinute, wordsPerChunk);
-	}
+export class SpeedReader extends HTMLElement {
 
 	constructor() {
 		super();
-		this._paragraphs = new BoundedList();
-		this._wordsPerMinute = 300;
-		this._charactersPerSecond = 1;
-		this._slightPauseEndPhrase = false;
-		this._paused = false;
-		this.updateCharactersPerSecond();
-		this.classList.add("speed-reader");
+		this.attachShadow({ mode: "open" });
 	}
 
-	get wordsPerMinute() {
-		return this._wordsPerMinute;
+	connectedCallback() {
+		this.shadowRoot.appendChild(templateContent(template));
 	}
 
-	set wordsPerMinute(wmp) {
-		this._wordsPerMinute = wmp;
-		this.updateCharactersPerSecond();
+	static get attrs() {
+		return attrs;
 	}
 
-	get charactersPerSecond() {
-		return this._charactersPerSecond;
+	static get cssVariables() {
+		return cssVariables;
 	}
 
-	get slightPauseEndPhrase() {
-		return this._slightPauseEndPhrase;
-	}
-
-	set slightPauseEndPhrase(enabled) {
-		this._slightPauseEndPhrase = enabled;
-	}
-
-	get paused() {
-		return this._paused;
-	}
-
-	set paused(is) {
-		this._paused = is;
-	}
-
-	get paragraphs() {
-		return this._paragraphs.list;
-	}
-
-	set paragraphs(list) {
-		this.paragraphs.forEach((paragraph) => paragraph.remove());
-		this._paragraphs.clear();
-		list.forEach((paragraph) => this.addParagraph(paragraph));
-	}
-
-	get currentParagraphIndex() {
-		return this._paragraphs.index;
-	}
-
-	set currentParagraphIndex(index) {
-		this._paragraphs.index = index;
-	}
-
-	get hasNextParagraph() {
-		return this._paragraphs.hasNext;
-	}
-
-	get hasPreviousParagraph() {
-		return this._paragraphs.hasPrevious;
-	}
-
-	newText(text, wordsPerMinute, wordsPerChunk) {
-		const chunkLength = wordsPerChunk * SpeedReader.averageWordSize;
-		this.wordsPerMinute = wordsPerMinute;
-		this.paragraphs = splitParagraphs(text)
-			.map((paragraph) => {
-				return ParagraphSpeedReader.build(
-					splitParagraphChunks(paragraph, chunkLength).map(ChunkText.build),
-				);
-			});
-		return this;
-	}
-
-	updateCharactersPerSecond() {
-		return this._charactersPerSecond = (
-			this.wordsPerMinute / 60 * SpeedReader.averageWordSize
-		);
-	}
-
-	addParagraph(chunk) {
-		this.appendChild(chunk);
-		this._paragraphs.add(chunk);
-	}
-
-	nextParagraph() {
-		return this._paragraphs.next();
-	}
-
-	previousParagraph() {
-		return this._paragraphs.previous();
-	}
-
-	async startSpeedReading() {
-		while (this.hasNextParagraph) {
-			const paragraph = this.nextParagraph();
-			while (paragraph.hasNextChunk) {
-				paragraph.nextChunk();
-				await sleep(
-					chunkMilliseconds(
-						paragraph.currentChunkLength,
-						this.charactersPerSecond,
-					),
-				);
-			}
-			paragraph.unhighlightCurrentChunk();
-			if (this.slightPauseEndPhrase) {
-				await sleep(300);
-			}
-			while (this.paused) {
-				await sleep(100);
-			}
-		}
-	}
 }
 
-const isDefined = (
-	customElements.get(SpeedReader.customTagName) !== undefined
-);
-if (!isDefined) {
-	customElements.define(
-		SpeedReader.customTagName,
-		SpeedReader,
-		{ extends: SpeedReader.extendingTagName },
-	);
+/**
+ * @returns {SpeedReader}
+ */
+export function buildSpeedReader() {
+	return tag({
+		tagName: "speed-reader",
+	});
 }
+
+customElements.define("speed-reader", SpeedReader);
