7e7e217e9cfef00b5cbd9f6ce0e83b8f41c35f07
Willian Carlos <wcs777git@outlook.com>
2023-02-11 21:33:10 -0300

    add defaultSettings

---
+++ js/utils/defaultSettings.js
@@ -0,0 +1,14 @@
+const defaultSettings = {
+	wordsPerMinute: 300,
+	wordsPerChunk: 6,
+	backgroundColor: "#FFFFFF",
+	textColor: "#B3B3B3",
+	highlightColor: "#000000",
+	fontSize: 16,
+	lineHeight: 1.5,
+	fontFamily: "Arial",
+	alignment: "left",
+	slightPause: true,
+};
+
+export default defaultSettings;
