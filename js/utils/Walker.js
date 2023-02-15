99c03b843faa10f850a10961ae78ab74ae845e94
Willian Carlos <wcs777git@outlook.com>
2023-02-15 18:25:43 -0300

    add Walker

---
+++ js/utils/Walker.js
@@ -0,0 +1,121 @@
+import { threshold } from "./alphanumeric.js";
+
+export default class Walker extends Array {
+
+	constructor(...args) {
+		super(...args);
+		this._index = -1;
+		this.indexChangeCallback = null;
+	}
+
+	/**
+	 * @param {(
+	 * 	oldCurrent: { index: number; value: any; },
+	 * 	newCurrent: { index: number; value: any; },
+	 * ) => void} indexChangeCallback
+	 */
+	static fromWithIndexChangeCallback(
+		indexChangeCallback, arrayLike, mapFn, thisArg
+	) {
+		const walker = Walker.from(arrayLike, mapFn, thisArg);
+		walker.indexChangeCallback = indexChangeCallback;
+		return walker;
+	}
+
+	/**
+	 * @param {(
+	 * 	oldCurrent: { index: number; value: any; },
+	 * 	newCurrent: { index: number; value: any; },
+	 * ) => void} indexChangeCallback
+	 */
+	static ofWithIndexChangeCallback(indexChangeCallback, ...items) {
+		const walker = Walker.of(items);
+		walker.indexChangeCallback = indexChangeCallback;
+		return walker;
+	}
+
+	get index() {
+		return this._index;
+	}
+
+	set index(index) {
+		const i = parseInt(index);
+		if (this.index !== i) {
+			const oldIndex = this.index;
+			const oldValue = this.current;
+			this._index = threshold(-1, i, this.length);
+			this.indexChangeCallback?.(
+				{ index: oldIndex, value: oldValue },
+				{ index: this.index, value: this.current },
+			);
+		}
+	}
+
+	get current() {
+		return this[this.index];
+	}
+
+	toIndex(index) {
+		this.index = index;
+		return this.current;
+	}
+
+	toFirst() {
+		return this.toIndex(0);
+	}
+
+	toLast() {
+		return this.toIndex(this.length - 1);
+	}
+
+	next() {
+		return this.toIndex(this.index + 1);
+	}
+
+	previous() {
+		return this.toIndex(this.index - 1);
+	}
+
+	rewind() {
+		this.index = -1;
+	}
+
+	*traverseForward() {
+		for (
+			let item = this.toFirst();
+			!this.isAfterLast();
+			item = this.next()
+		) {
+			console.log({ yielding: item });
+			yield item;
+		}
+	}
+
+	*traverseBackward() {
+		for (
+			let item = this.toLast();
+			!this.isBeforeFirst();
+			item = this.previous()
+		) {
+			console.log({ yielding: item });
+			yield item;
+		}
+	}
+
+	isBeforeFirst() {
+		return this.index < 0;
+	}
+
+	isAfterLast() {
+		return !(this.index < this.length);
+	}
+
+	hasPrevious() {
+		return this.index > 0;
+	}
+
+	hasNext() {
+		return this.index + 1 < this.length;
+	}
+
+}
