ee99346a20784152ba9dbed74a392648e21d4d56
Willian Carlos <wcs777git@outlook.com>
2023-02-24 11:10:30 -0300

    add buttons listeners

--- js/reader.js
+++ js/reader.js
@@ -1,2 +1,23 @@
 import { CustomModal } from "./components/CustomModal.js";
 import { SpeedReader } from "./components/SpeedReader.js";
+import { $$, byId } from "./utils/dom.js";
+
+const reader = byId("speed-reader");
+const read = byId("read");
+
+read.addEventListener("click", () => {
+	const toggle = !reader.isPaused;
+	reader.isPaused = toggle;
+	read.textContent = reader.isPaused ? "Read" : "Paused";
+	read.blur();
+});
+for (const modal of $$('[is=custom-modal]')) {
+	let isPaused = true;
+	modal.addEventListener("modal-opened", () => {
+		isPaused = reader.isPaused;
+		reader.isPaused = true;
+	});
+	modal.addEventListener("modal-closed", () => {
+		reader.isPaused = isPaused;
+	});
+}
